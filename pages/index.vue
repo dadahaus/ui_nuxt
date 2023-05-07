39a01d704bc8239225e406e2ab6dc6e1d14d1c3d
ostin <casadadaista@gmail.com>
2023-05-07 19:28:03 +0200

    tailwind working

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="max-w-5xl mx-auto py-16 px-4 dark">
-    <Colors title="Colors"> </Colors>
+  <div class="max-w-5xl bg-green-500 mx-auto py-16 px-4 dark">
+    <Colors title="Colors">
+      <div class="w-8 h-8 bg-green-500 rounded-md"></div>
+    </Colors>
   </div>
 </template>
