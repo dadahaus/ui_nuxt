32c401c22b099f1eb526bc801d7fae7eb398c799
ostin <casadadaista@gmail.com>
2023-05-08 12:38:16 +0200

    palette

--- components/Palette.vue
+++ components/Palette.vue
@@ -1,21 +1,24 @@
 <template>
-  <div
-    style="background-color: var(--color-primary)"
-    class="w-16 h-16 rounded-md"
-  ></div>
-  <div
-    style="background-color: var(--color-secondary)"
-    class="w-16 h-16 rounded-md"
-  ></div>
-  <div
-    style="background-color: var(--color-secondary-one)"
-    class="w-16 h-16 rounded-md"
-  ></div>
-  <div
-    style="background-color: var(--color-secondary-two)"
-    class="w-16 h-16 rounded-md"
-  ></div>
-  <div class="w-16 h-16 bg--700 rounded-md"></div>
+  <div class="flex flex-col basis-1/3 gap-8">
+    <div
+      style="background-color: var(--color-primary)"
+      class="w-16 h-16 rounded-md"
+    ></div>
+    <div
+      style="background-color: var(--color-secondary)"
+      class="w-16 h-16 rounded-md"
+    ></div>
+    <div
+      style="background-color: var(--color-secondary-one)"
+      class="w-16 h-16 rounded-md"
+    ></div>
+    <div
+      style="background-color: var(--color-secondary-two)"
+      class="w-16 h-16 rounded-md"
+    ></div>
+    <div class="w-16 h-16 bg--700 rounded-md"></div>
+  </div>
+
   <span>
     <slot />
   </span>
